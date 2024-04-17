94e937a8e7ea01f269d2cc48d5c1901614e74bd7
Nate Choe <nate@natechoe.dev>
2024-04-17 15:26:51 -0500

    Basic city block class templates

---
+++ BetaCityMap.java
@@ -0,0 +1,12 @@
+// TODO: Replace this with an actual implementation of a CityMap
+
+import mayflower.*;
+
+public class BetaCityMap extends CityMap {
+    public CityBlock getBlock(int x, int y) {
+        if (x < 0 || x >= 2 || y < 0 || y >= 2) {
+            return null;
+        }
+        return new StaticCityBlock("images/office.png", 0, 120);
+    }
+}
