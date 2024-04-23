36894ac53d0d6d29056b034f27f07ec9ecc91673
Nate Choe <nate@natechoe.dev>
2024-04-23 15:08:06 -0500

    addObject (throws NullPointerException)

--- MyWorld.java
+++ MyWorld.java
@@ -7,6 +7,7 @@ public class MyWorld extends World
     public MyWorld(){
         setBackground("images/background.png");
         renderer = new CityRenderer(new BetaCityMap(), 800, 600, -100, -100);
+        addObject(renderer, 0, 0);
     }
     
     public void act(){
