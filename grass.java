435faab95c48c0cd500c86e095df6cace247c2dd
Arif <arifrcamci@gmail.com>
2024-04-23 15:10:19 -0500

    grass

--- grass.java
+++ grass.java
@@ -1,33 +1,34 @@
 
-/**
- * Write a description of class grass here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class grass
+import mayflower.*;
+public class grass extends CityBlock
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    
+    private MayflowerImage p;
     /**
      * Constructor for objects of class grass
      */
     public grass()
     {
-        // initialise instance variables
-        x = 0;
+        p = new MayflowerImage("grass.png");
+        
+        //setImage(p);
+    }
+    public MayflowerImage getImage(){
+        return p;
+        
+    }
+    public int getXOffset(){
+        //return getX();
+        return 0;
+    }
+    public int getYOffset(){
+        //return getY();
+        return 0;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void act(){
+        if(Mayflower.mouseClicked(this)){
+            
+        }
     }
 }
