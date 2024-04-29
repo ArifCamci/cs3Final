e705114cb53c8ba6770d7412df2046e1f99d8f7c
Arif <arifrcamci@gmail.com>
2024-04-29 14:57:11 -0500

    myworld back to normal

--- MyWorld.java
+++ MyWorld.java
@@ -6,8 +6,9 @@ public class MyWorld extends World
 
     public MyWorld(){
         setBackground("images/background.png");
-        money x = new money(10, 1);
-        System.out.println(x.CalcMoney());
+        renderer = new CityRenderer(new BetaCityMap(), 800, 600, -100, -100);
+        addObject(renderer, 0, 0);
+        addObject(new HUD(), 0, 0);
     }
     
     public void act(){
